bdb36d93622ff3f028a91958205f38ae4982b343
RomanVetrov <romanvikernes@gmail.com>
2026-02-03 13:21:28 +0300

    Инициализация, настройка базы, переменных окружения, конфига

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from app.config import settings
+from collections.abc import AsyncGenerator
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True, # логи
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db() -> AsyncGenerator[AsyncSession]: # асинхронно генерит асинк сессию
+    async with AsyncSessionLocal() as session:
+        yield session
