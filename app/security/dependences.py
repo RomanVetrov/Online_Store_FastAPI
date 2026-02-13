0668b92338ab6bdb0afa4bcf325a95c3f855e436
RomanVetrov <romanvikernes@gmail.com>
2026-02-13 22:05:15 +0300

    fix(auth): исправить OAuth2 tokenUrl для Swagger
    - tokenUrl обновлен на /api/v1/auth/login.
    - Кнопка Authorize в /docs теперь использует корректный путь логина.

--- app/security/dependences.py
+++ app/security/dependences.py
@@ -9,7 +9,7 @@ from app.models.user import User
 from app.repositories.user_repo import get_user_by_id
 from app.security.jwt import decode_access_token, TokenExpired, TokenInvalid
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
 
 
 async def get_current_user(
